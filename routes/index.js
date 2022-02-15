018bf1dec5b1675724deb97fa2a8935cc6a54523
Watts-Blake <watts.blake52@gmail.com>
2022-02-14 22:14:59 -0800

    ran in to store bug, currently resolving

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 // PACKAGE IMPORTS ******************************************************************
-var express = require('express');
-const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
+var express = require("express");
+const bcrypt = require("bcryptjs");
+// const asyncHandler = require('express-async-handler');
 // MODULE IMPORTS *******************************************************************
-const { loginUser, restoreUser, requireAuth, logoutUser } = require('../auth');
-const db = require('../db/models');
+const { loginUser, restoreUser, requireAuth, logoutUser } = require("../auth");
+const db = require("../db/models");
 
 // MIDDLEWARE ***********************************************************************
 var router = express.Router();
@@ -14,14 +14,16 @@ router.use(restoreUser);
 // ROUTER ***************************************************************************
 
 // GET /
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Audio Hunt', authenticated: res.locals.authenticated });
+router.get("/", function (req, res, next) {
+  return res.render("index", {
+    title: "Audio Hunt",
+    authenticated: res.locals.authenticated,
+  });
 });
 
 // GET /random
-router.get('/random', requireAuth, (req, res) => {
-  res.send('urniujknfvedjnkrcszfvbzdbxgdfgxcfvefdvsebfdgg');
+router.get("/random", requireAuth, (req, res) => {
+  res.send("urniujknfvedjnkrcszfvbzdbxgdfgxcfvefdvsebfdgg");
 });
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 // PACKAGE IMPORTS ******************************************************************
-var express = require('express');
-const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
+var express = require("express");
+const bcrypt = require("bcryptjs");
+// const asyncHandler = require("express-async-handler");
 
 // MODULE IMPORTS *******************************************************************
-const { loginUser, restoreUser, requireAuth, logoutUser } = require('../auth');
-const db = require('../db/models');
-
+const { loginUser, restoreUser, requireAuth, logoutUser } = require("../auth");
+const db = require("../db/models");
+const asyncHandler = (handler) => (req, res, next) =>
+  handler(req, res, next).catch(next);
 // MIDDLEWARE ***********************************************************************
 var router = express.Router();
 
@@ -14,64 +15,67 @@ router.use(restoreUser);
 
 // ROUTES *****************************************************************
 // GET /users *************************************************************
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 // GET /users/login
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 // POST /users/login
-router.post('/login', asyncHandler(async (req, res) => {
-  const {
-      email,
-      password,
-  } = req.body;
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-  const user = await db.User.findOne({ where: { email } });
+    const user = await db.User.findOne({ where: { email } });
 
-  if(user !== null){
-    const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+    if (user !== null) {
+      const passwordMatch = await bcrypt.compare(
+        password,
+        user.hashedPassword.toString()
+      );
 
-    if(passwordMatch) {
-      loginUser(req, res, user);
+      if (passwordMatch) {
+        loginUser(req, res, user);
 
-      return res.redirect('/');
+        return res.redirect("/");
+      }
     }
-  }
 
-  res.render('login', { email });
-}));
+    return res.render("login", { email });
+  })
+);
 // POST /users/logout
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   logoutUser(req, res);
 
-  res.redirect('/');
+  return res.redirect("/");
 });
 // GET /users/signup
-router.get('/signup', (req, res) => {
-  res.render('signup', { user: {} });
+router.get("/signup", (req, res) => {
+  return res.render("signup", { user: {} });
 });
 // POST /users/signup
-router.post('/signup', asyncHandler(async (req, res, next) => {
-  const { username, email, bio, password, confirmPassword } = req.body;
-  // TODO: implement express-validator
+router.post(
+  "/signup",
+  asyncHandler(async (req, res, next) => {
+    const { username, email, bio, password, confirmPassword } = req.body;
+    // TODO: implement express-validator
 
-  const user = db.User.build({ username, email, bio });
+    const user = db.User.build({ username, email, bio });
 
-  if (password !== confirmPassword) {
-    res.render('signup', user);
-  }
+    if (password !== confirmPassword) {
+      res.render("signup", user);
+    }
 
-  const hashedPassword = await bcrypt.hash(password, 12);
-  user.hashedPassword = hashedPassword;
-  await user.save();
+    const hashedPassword = await bcrypt.hash(password, 12);
+    user.hashedPassword = hashedPassword;
+    await user.save();
 
-  loginUser(req, res, user);
-  res.redirect('/');
-}));
-
-
-
+    loginUser(req, res, user);
+    return res.redirect("/");
+  })
+);
 
 module.exports = router;
