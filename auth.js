9ba9b306424719f6318062334d23c278cc19c735
nick-esqueda <nick_esqueda@outlook.com>
2022-02-14 19:16:55 -0600

    working on logging out user (already did other auth functions)

---
+++ auth.js
@@ -0,0 +1,60 @@
+// log in user
+// log out user
+// requireAuth
+// restore user
+const db = require('./db/models');
+
+const loginUser = (req, res, user) => {
+  req.session.auth = {
+    userId: user.id,
+  }
+};
+
+const logoutUser = (req, res) => {
+  // TODO go to DB and delete session from session table?
+  delete req.session.auth;
+};
+
+const requireAuth = (req, res, next) => {
+  if (!res.locals.authenticated) {
+    return res.redirect('/');
+  }
+  return next();
+};
+
+const restoreUser = async (req, res, next) => {
+  console.log(req.session);
+
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+
+    try {
+      const user = await db.User.findByPk(userId);
+
+      if(user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
+        next();
+      }
+    } catch (error) {
+      res.locals.authenticated = false;
+      next(error);
+    }
+
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
+module.exports = {
+  loginUser,
+  logoutUser,
+  requireAuth,
+  restoreUser,
+};
+
+
+
+// requireAuth middleware.....
+// POST /comments
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('../db/models');
+const asyncHandler = require('express-async-handler');
+
+const { loginUser, restoreUser, requireAuth } = require('../auth');
+
+// MIDDLEWARE ***********************************************************************
+router.use(restoreUser);
+
+// ROUTER ***************************************************************************
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  res.render('index', { title: 'Audio Hunt', authenticated: res.locals.authenticated });
 });
+
+// TEST ROUTE TO LOG IN USER
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.post('/login', asyncHandler(async (req, res) => {
+  const {
+      email,
+      password,
+  } = req.body;
+
+  const user = await db.User.findOne({ where: { email } });
+
+  if(user !== null){
+    const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+    if(passwordMatch) {
+      loginUser(req, res, user);
+
+      return res.redirect('/');
+    }
+  }
+
+  res.render('login', { email });
+}));
+
+router.get('/random', requireAuth, (req, res) => {
+  res.send('urniujknfvedjnkrf;vebfdgg');
+});
+
 
 module.exports = router;
