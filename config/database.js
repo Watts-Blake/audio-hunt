93d656a74c05584e51be499a6942ce9f27d1199b
Watts-Blake <watts.blake52@gmail.com>
2022-02-17 08:09:59 -0800

    heroku test

--- config/database.js
+++ config/database.js
@@ -14,12 +14,6 @@ module.exports = {
     production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    dialectOptions: {
-   ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    },
     seederStorage: 'sequelize',
   }
 };
