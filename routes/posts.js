414e6a49f6d355c738043e27291bf846c2d1d376
nick-esqueda <nick_esqueda@outlook.com>
2022-02-18 00:44:24 -0600

    implemented comment creation (still need to work on validations/error container

--- routes/posts.js
+++ routes/posts.js
@@ -19,6 +19,7 @@ const csrfProtection = csrf({ cookie: true });
 // GET /posts/:id
 router.get(
   '/:id(\\d+)',
+  csrfProtection,
   asyncHandler(async (req, res, next) => {
     const id = (await req.params.id) * 1;
 
@@ -48,6 +49,7 @@ router.get(
         loggedInUser,
         timeElapsed,
         isAuthorized,
+        csrfToken: req.csrfToken(),
       });
     } else {
       const error = new Error("We could not find this post!");
@@ -95,7 +97,7 @@ router.post('/new',
       where: {
         songName,
         artistName
-      } 
+      }
     })
 
     let errors = []
